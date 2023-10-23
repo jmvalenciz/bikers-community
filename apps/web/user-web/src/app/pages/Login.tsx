4cec0af03f9c01d885c3afe7f773f6e95bb5bde0
Sebastian OM <sebastianobandomorales@gmail.com>
2023-10-23 07:38:10 -0500

    adding login

---
+++ apps/web/user-web/src/app/pages/Login.tsx
@@ -0,0 +1,30 @@
+import { Logo, FormRow } from "@bikers-community/ui"
+import Wrapper from "@bikers-community/ui";
+import { Link } from "react-router-dom";
+
+const Login: React.FC = () => {
+  return (
+    <Wrapper>
+      <form className="form">
+        <Logo />
+        <h4>Login</h4>
+        <FormRow type="email" name="email"/>
+        <FormRow type="password" name="password"/>
+        <button type="submit" className="btn btn-block">
+          submit
+        </button>
+        <button type="button" className="btn btn-block">
+          explore the app
+        </button>
+        <p>
+          Not a member yet?
+          <Link to="/register" className="member-btn">
+            Register
+          </Link>
+        </p>
+      </form>
+    </Wrapper>
+  );
+};
+
+export default Login;

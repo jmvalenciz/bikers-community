7f79b86696817990acf18682d235bfbbd1410776
felipealvarezb <felipealvarezb1@gmail.com>
2023-10-24 11:32:24 -0500

    initial event service

--- apps/microservices/events/src/app/app.controller.ts
+++ apps/microservices/events/src/app/app.controller.ts
@@ -2,12 +2,15 @@ import { Controller, Get } from '@nestjs/common';
 
 import { AppService } from './app.service';
 
+import { Event } from '@bikers-community/models';
+
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getData() {
-    return this.appService.getData();
+  getData() : Event{
+    const Event: Event = {createdByAdminId:'', eventId:'', eventName:'', description:'', eventDate:''};
+    return Event;
   }
 }
--- lib/models/src/index.ts
+++ lib/models/src/index.ts
@@ -1,3 +1,4 @@
 export * from './bike';
 export * from './general/object_id';
 export * from './booking';
+export * from './events';

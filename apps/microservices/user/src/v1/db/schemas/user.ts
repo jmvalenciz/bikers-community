2ce7c026d485018bda0147e25b28d41cf0b1f690
Deivis Vargas <tech@cparking.com.co>
2023-10-27 21:08:20 -0500

    Add columns to table

--- apps/microservices/user/src/v1/db/schemas/user.ts
+++ apps/microservices/user/src/v1/db/schemas/user.ts
@@ -1,28 +1,31 @@
 import { User } from '@bikers-community/models';
 import { model, Schema } from 'mongoose';
 
-const UserSchema = new Schema<User>({
-  createdByAdminId: {
-    type: Schema.ObjectId,
-    required: true,
-  },
-  updatedByAdminId: { 
-    type: Schema.ObjectId,
-    required: true,
-  },
-  createdAt: {
-    type: Date,
-    default: new Date(),
-  },
-  updatedAt: {
-    type: Date,
-    default: new Date()
-  },
-  userName: {
-    type: String,
-    required: true
-  }
-}, {
+const UserSchema = new Schema<User>(
+  {
+    
+    createdAt: {
+      type: Date,
+      default: new Date(),
+    },
+    updatedAt: {
+      type: Date,
+      default: new Date()
+    },
+    userName: {
+      type: String,
+      required: true
+    },
+    accessToken: {
+      type: String,
+      required: true,
+    },
+    authProvider: {
+      type: String,
+      required: true,
+    }
+   
+  }, {
   minimize: false,
   versionKey: false,
   autoIndex: false,
--- lib/models/src/user.ts
+++ lib/models/src/user.ts
@@ -4,16 +4,11 @@ import { ObjectIdSchema } from './general/object_id';
 export const User = z.object({
   _id: ObjectIdSchema,
   userName: z.string().min(3).max(250),
-  createdByAdminId: ObjectIdSchema,
-  updatedByAdminId: ObjectIdSchema,
+  accessToken: z.string().min(3).max(250),
+  authProvider: z.string().min(3).max(250),
   updatedAt: z.date(),
   createdAt: z.date()
 });
-
-export const UserData = z.object({
-  createdAt: z.date()
-});
-
 
 export const NewUser = User.omit({
   _id: true,
@@ -21,14 +16,12 @@ export const NewUser = User.omit({
   createdAt:true
 });
 export const UpdateUser = User.omit({
-  userId: true,
-  userName: true,
-  updatedByAdminId: true,
-  updatedAt: true
+  _id: true,
+  updatedAt:true,
+  createdAt:true
 });
 
 export type User = z.infer<typeof User>;
-export type UserData = z.infer<typeof UserData>; 
 
 export type NewUser = z.infer<typeof NewUser>;
 export type UpdateUser = z.infer<typeof UpdateUser>; 
